f71521fc998cf3cd7cc4b64b5ce8c2a389b4574a
Harley McKee <h@harley.website>
2018-06-19 22:06:28 -0500

    some state

--- assets/js/HelloWorld.js
+++ assets/js/HelloWorld.js
@@ -1,10 +1,42 @@
 import React, {Component, createElement} from 'react'
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+})
 
 class HelloWorld extends Component {
-  render() {
-    const {name} = this.props
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: initialValue,
+    }
+  }
 
-    return <div>Hello {name}</div>
+  onChange({value}) {
+    this.setState({value})
+  }
+
+  render() {
+    return <Editor value={this.state.value} onChange={this.onChange} />
   }
 }
 
